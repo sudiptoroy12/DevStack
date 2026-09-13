47c2f080f1d8444b421a5bd0fa4cd012bf16b555
sudiptoroy12 <sudipto1294@gmail.com>
2026-09-13 19:58:02 +0600

    Refactor Techonologies component layout for improved responsiveness and grid structure

--- src/components/techonologies/Techonologies.tsx
+++ src/components/techonologies/Techonologies.tsx
@@ -21,11 +21,11 @@ const [selectedTechnologies, setSelectedTechnologies] = useState<ITechnology[]>(
         <div className="mx-auto max-w-7xl ">  
         <h1 className='text-4xl font-bold px-6 pb-3 text-center sm:text-left'>Explore the <span className='brand-gradient'>Technologies</span></h1>
         <h3 className='text-lg text-gray-600 px-6 text-center sm:text-left'>Pick one technology per category to build your ideal stack.</h3>
-        <div className="grid grid-cols-1   gap-6 md:grid-cols-4  ">
-            <div className="col-span-1 md:col-span-3  flex"> 
+        <div className="grid grid-cols-1   gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  ">
+            <div className="col-span-1 sm:col-span-2 md:col-span-3 lg:col-span-3  flex"> 
                 <AvailableTechonologies  techonologiesPromise={techonologiesPromise} selectedTechnologies={selectedTechnologies} setSelectedTechnologies={setSelectedTechnologies} />
             </div>
-            <div className="col-span-1 flex justify-center md:block md:mt-12 mb-12  md:mb-0"> 
+            <div className="md:col-span-1  flex justify-center md:block md:mt-12 mb-12  md:mb-0"> 
             <SelectedTechonologies selectedTechnologies={selectedTechnologies} setSelectedTechnologies={setSelectedTechnologies} />
             </div>
         </div>
